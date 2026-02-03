04bd9320f8312ebe1b9f5be7303406a6cf815fb7
Vishnu KR <vishnukr1995@gmail.com>
2026-02-03 15:32:20 +0530

    Create outputs.tf

---
+++ vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnets" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnets" {
+  value = [for s in aws_subnet.private : s.id]
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.this.id
+}
