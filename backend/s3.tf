0fb11b37e827d44a1556dca0b3d01a0b0032f9f5
Vishnu KR <vishnukr1995@gmail.com>
2026-02-03 15:19:21 +0530

    Update s3.tf

--- backend/s3.tf
+++ backend/s3.tf
@@ -1,9 +1,39 @@
-terraform {
-  backend "s3" {
-    bucket         = "demo-terraform-eks-vkr6677-state-s3-bucket"
-    key            = "platform/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-eks-state-locks"
-    encrypt        = true
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "demo-terraform-eks-vkr6677-state-s3-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-eks-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
